7594952a822b62155d44a2350317aede39f85a60
captaincactuz <emanmomot@gmail.com>
2014-09-05 19:06:48 -0400

    some setup

---
+++ Assets/playMov.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playMov : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		((MovieTexture)renderer.material.mainTexture).Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
